ea2429d47e0ae418b3fd6a0de312014378f83fbc
Alexander <ablancoa95@gmail.com>
2023-03-23 17:21:09 -0300

    [UPDATE] portada image

--- src/containers/HomePage/HeaderContent.jsx
+++ src/containers/HomePage/HeaderContent.jsx
@@ -1,18 +1,14 @@
 import React, { useRef } from 'react';
-import { AdvancedImage } from '@cloudinary/react';
-import { placeholder, responsive } from '@cloudinary/react';
-import { quality } from '@cloudinary/url-gen/actions/delivery';
 import SendRoundedIcon from '@mui/icons-material/SendRounded';
 import Button from '../../components/Button';
-import { cld } from '../../utils/cloudinary'
 import { useObserver } from '../../hooks/useObserver';
+import portada from '../../assets/images/portada.png'
 import apiIcon from '../../assets/Icons/api-icon.svg';
 import codeSnippet from '../../assets/Icons/code-snippet.svg';
 import codeSnippet1 from '../../assets/Icons/code-snippet-1.svg';
 import '../../styles/homePage/HeaderContent.scss'
 
 export default function HeaderContent() {
-  const portadaImage = cld.image('Portfolio/portada_400');
 
   const imageContainer = useRef();
   const animation = useObserver(imageContainer, "EntryAnimationHeaderHome")
@@ -25,7 +21,7 @@ export default function HeaderContent() {
         <Button iconComponent={SendRoundedIcon} text={`Contáctame`} urlToGo={`/contact`} />
       </div>
       <div className={`Header-image`} >
-        <AdvancedImage cldImg={portadaImage} className='myImage' rel="preload" alt="Alexander Blanco Image" plugins={[responsive(400, 350), placeholder()]} />
+        <img src={portada} className='myImage' rel="preload" alt="Alexander Blanco portada" />
         <img src={apiIcon} alt='icono de api' className='apiIcon' />
         <img src={codeSnippet} className='codeSnippet' alt='Open Init' />
         <img src={codeSnippet1} className='codeSnippet1' alt='close Snippet' />
